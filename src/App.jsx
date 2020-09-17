b6931f6ecbd209121aedd817fc04a17929f16944
faslash <bicadete@hotmail.com>
2020-09-17 13:59:43 -0300

    Aula 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
@@ -8,6 +9,7 @@ export default props => {
     return (
         <div id="app">
             <h1>Fundamentos React (arrow)</h1>
+            <Aleatorio min={1} max={10}/>
             <Fragmento />
             <ComParametro
                 titulo="Situação do Aluno" aluno="PedroSilva"
---
+++ src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default props => {
+    const {min, max} = props;
+
+    const aleatorio = parseInt(Math.random() * (max - min) + min);
+    return(
+    <div>
+        <h2>Valor Aleatório</h2>
+        <p><strong>Valor mínimo: </strong> {min}</p>
+        <p><strong>Valor maximo: </strong> {max}</p>
+        <p><strong>Valor Escolhido: </strong> {aleatorio}</p>
+    </div>
+    )
+}
