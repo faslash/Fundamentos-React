f370b46cd740a01ca003da50b91f68c21d6d43d7
faslash <bicadete@hotmail.com>
2020-09-17 23:40:48 -0300

    Aula 19

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,30 @@ import Aleatorio from './components/basicos/Aleatorio'
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import Card from './components/layout/Card';
 
 export default props => {
     return (
         <div id="app">
             <h1>Fundamentos React (arrow)</h1>
-            <Aleatorio min={1} max={10}/>
-            <Fragmento />
-            <ComParametro
-                titulo="Situação do Aluno" aluno="PedroSilva"
-                nota={9.3} />
-            <Primeiro></Primeiro>
+            <Card titulo="Desafio Aleatorio">
+                <Aleatorio min={1} max={10} />
+            </Card>
+
+            <Card titulo="Fragmento">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno" aluno="PedroSilva"
+                    nota={9.3} />
+            </Card>
+
+            <Card titulo="Primeiro Componente">
+                <Primeiro></Primeiro>
+            </Card>
+
         </div>
     )
 }
